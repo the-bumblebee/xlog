7b73044149d259b5cdf9bfc664c9410d2aa6aba9
Asif Nassar <klmktu.asif@gmail.com>
2025-06-08 21:14:40 +0530

    Enhance client secret loading for flexibity

--- src/xlog/config.py
+++ src/xlog/config.py
@@ -1,8 +1,11 @@
 import os
 import dotenv
+import importlib.resources
 
 dotenv.load_dotenv()
-GOOGLE_CLIENT_PATH = os.getenv('GOOGLE_CLIENT_PATH', os.path.join(os.path.dirname(__file__), 'secrets', 'client_secrets.json'))
+_RESOURCES_PATH = importlib.resources.files('xlog').joinpath('resources')
+
+GOOGLE_CLIENT_PATH = os.getenv('GOOGLE_CLIENT_PATH', _RESOURCES_PATH.joinpath('client_secret.json'))
 GOOGLE_TOKEN_PATH = os.getenv('GOOGLE_TOKEN_PATH', os.path.join(os.path.dirname(__file__), 'secrets', 'token.json'))
 SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
 SHEET_NAME = os.getenv('SHEET_NAME')
