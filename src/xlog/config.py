01dcae8135cce4a56d58ceb0360e8af743af313a
Asif Nassar <klmktu.asif@gmail.com>
2025-06-11 01:06:58 +0530

    Refactor config.py to expose config related methods

--- src/xlog/config.py
+++ src/xlog/config.py
@@ -6,12 +6,15 @@ from platformdirs import user_config_dir
 
 dotenv.load_dotenv()
 
-def _get_config_path(config_base_dir):
-    os.makedirs(config_base_dir, exist_ok=True)
-    config_file_path = os.path.join(config_base_dir, "config.yaml")
-    return config_file_path
+_RESOURCES_PATH = importlib.resources.files('xlog').joinpath('resources')
+_CONFIG_BASE_DIR = user_config_dir(appname="xlog")
 
-def _load_config(config_file_path = ""):
+def _get_config_file_path(config_base_dir = _CONFIG_BASE_DIR):
+    os.makedirs(config_base_dir, exist_ok=True)
+    return os.path.join(config_base_dir, "config.yaml")
+
+def load_config():
+    config_file_path = _get_config_file_path()
     if not os.path.exists(config_file_path):
         return {}
     try:
@@ -22,7 +25,16 @@ def _load_config(config_file_path = ""):
         exit(1)
     return config
 
-def _get_value_from_config(config, key):
+def dump_config(config):
+    config_file_path = _get_config_file_path()
+    try:
+        with open(config_file_path, 'w') as config_file:
+            yaml.safe_dump(config, config_file, default_flow_style=False)
+    except yaml.YAMLError as e:
+        print(f"Error dumping config file: {e}")
+        exit(1)
+
+def get_value_from_config(config, key):
     keys = key.split('.')
     value = config
     for k in keys:
@@ -32,12 +44,9 @@ def _get_value_from_config(config, key):
             raise KeyError(f"Key '{key}' not found in config")
     return value
 
-_RESOURCES_PATH = importlib.resources.files('xlog').joinpath('resources')
-_CONFIG_BASE_DIR = user_config_dir(appname="xlog")
-_CONFIG = _load_config(_get_config_path(_CONFIG_BASE_DIR))
-
-CONFIG_PATH = _get_config_path(_CONFIG_BASE_DIR)
+CONFIG = load_config()
 GOOGLE_CLIENT_PATH = os.getenv('GOOGLE_CLIENT_PATH', _RESOURCES_PATH.joinpath('client_secrets.json'))
-GOOGLE_TOKEN_PATH = os.getenv('GOOGLE_TOKEN_PATH') or _get_value_from_config(_CONFIG, 'google.token_path')
-SPREADSHEET_ID = os.getenv('SPREADSHEET_ID') or _get_value_from_config(_CONFIG, 'google.spreadsheet_id')
+# GOOGLE_TOKEN_PATH = os.getenv('GOOGLE_TOKEN_PATH') or get_value_from_config(CONFIG, 'google.token_path')
+GOOGLE_TOKEN_PATH = ""
+SPREADSHEET_ID = os.getenv('SPREADSHEET_ID') or get_value_from_config(CONFIG, 'google.spreadsheet_id')
 SHEET_NAME = os.getenv('SHEET_NAME')
